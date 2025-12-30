814cf37de99d60078b56a4a2e3b902038e142395
Lakshmi BL <lakshmi.bl@prodapt.com>
2025-12-30 12:17:29 +0530

    feat: adding Service account in sbx

--- sbx/vpc/main.tf
+++ sbx/vpc/main.tf
@@ -9,3 +9,12 @@ module "vpc" {
   shared_vpc_host = false
   bgp_best_path_selection_mode = "STANDARD"
 }
+
+module "colt_sbx_sa" {
+  source = "colt/modules/service-account"
+
+  project_id   = var.project_id
+  name         = "colt-sbx-sa"
+  display_name = "Colt Sandbox Service Account"
+  description  = "Service account for Colt sandbox workloads"
+}
