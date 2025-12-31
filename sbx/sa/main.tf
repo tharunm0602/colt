f998741a350bf9e3e424c175216dea788584a1d5
Lakshmi BL <lakshmi.bl@prodapt.com>
2025-12-31 15:21:51 +0530

    fix: adding the SA permissions

--- sbx/sa/main.tf
+++ sbx/sa/main.tf
@@ -4,4 +4,12 @@ module "colt_sbx_sa" {
   project_id   = var.project_id
   name         = var.sa_name
   display_name = var.display_name
+
+project_roles = [
+  "roles/logging.logWriter",
+  "roles/monitoring.editor",
+  "roles/iam.serviceAccountUser",
+  roles/storage.admin",
+  "roles/aiplatform.user"
+]
 }
