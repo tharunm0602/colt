e8f78933047384e5971734d75594efcb076c2d63
Mohammed <JMohammed@INTERNAL.COLT.NET>
2026-04-22 22:00:55 +0530

    another

---
+++ terraform/infra/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {}
+variable "envname" {}
+variable "region"  {}
+variable "data_disk_size_gb" { type = number }
+variable "data_disk_type" {}
+variable "machine_type" {}
+variable "boot_disk_size_gb" { type = number }
+variable "boot_disk_type" {}
+variable "cloud_run_service_name" {}
